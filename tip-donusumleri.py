123e4fd3a4e3e22db804978ec1e9016faf0f3f35
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-09-28 18:03:29 +0300

    first commit

---
+++ tip-donusumleri.py
@@ -0,0 +1,12 @@
+toplama_bir = input("birinci sayıyı giriniz: ");
+toplama_iki = input("ikinci sayıyı giriniz: ");
+
+print(type(toplama_bir));
+
+print(int(toplama_bir)+int(toplama_iki));
+
+print(float(12));
+
+print(int(12.53));
+
+print(type(str(12)));
