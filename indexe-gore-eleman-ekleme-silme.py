31a00c5017bc490bee08865ab92333318d94ee66
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-09-28 19:49:14 +0300

    indexe göre eleman ekleme silme oluşturuldu.

---
+++ indexe-gore-eleman-ekleme-silme.py
@@ -0,0 +1,19 @@
+liste = ["ali","veli","isik"]
+
+print(liste)
+
+liste.insert(0,"ayse")
+
+print(liste)
+
+liste.insert(len(liste),"beren")
+
+print(liste)
+
+liste.pop(0)
+
+print(liste)
+
+liste.pop(2)
+
+print(liste)
