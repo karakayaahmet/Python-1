960dc0b6892fe0787f59818434ecc8cd05ed0f10
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-09-28 22:47:22 +0300

    tuple oluşturma tamamlandı.

---
+++ tuple-olusturma.py
@@ -0,0 +1,12 @@
+# tuple:
+# 1) Kapsayıcıdır.
+# 2) Sıralıdır.
+# 3) Değiştirilemezlerdir.
+
+t= ("ali","veli",1,2,3,[10,20,30])
+
+t= "ali", "veli", 1,2,3, [10,20]
+
+t= ("eleman",)
+
+print(type(t))
