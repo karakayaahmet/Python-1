21279dac95cf377391b6403f74af943ef5e2ac7e
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-09-28 18:21:31 +0300

    liste oluştur oluşturuldu.

---
+++ liste-olusturma.py
@@ -0,0 +1,16 @@
+# listeler:
+# değiştirilebilir.
+# kapsayıcıdırlar.(farklı tipte veri tutabilir.)
+# sıralıdır.
+
+# liste oluşturma yolları
+# 1) []
+# 2) list()
+
+notlar = [90,60,70,50,40]
+print(type(notlar))
+
+liste = ["a",1,2,3]
+liste_genis = [liste,notlar]
+
+print(len(liste_genis))
