eb376e7b45907b842825f685f3d252d35ba27cde
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-09-29 13:01:51 +0300

    class oluşturma tamamlandı.

---
+++ class-olusturma.py
@@ -0,0 +1,12 @@
+class VeriBilimci():
+    def __init__(self):
+        self.bilinen_diller = []
+
+
+veli = VeriBilimci()
+veli.bilinen_diller = "python"
+
+ali = VeriBilimci()
+print(ali.bilinen_diller)
+
+print(veli.bilinen_diller)
